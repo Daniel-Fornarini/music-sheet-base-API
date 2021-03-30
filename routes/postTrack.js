2194eae878b7951612bc00ec495feefeeafc22e6
Daniel_Fornarini <44554840+Daniel-Fornarini@users.noreply.github.com>
2021-03-30 22:36:07 +0200

    deploy

--- routes/postTrack.js
+++ routes/postTrack.js
@@ -19,101 +19,91 @@ const s3 = new AWS.S3({
     region: process.env.AWS_REGION  
 });
 
-const storage = multer.memoryStorage({ //diskStorage()
-    destination: function(req, file, cb) { //multer esegue queste funzioni ogni volta che un file viene ricevuto
-        //cb(null, './uploads'); // errore e cartella dove mettere il file
+// const storage = multer.diskStorage({ //diskStorage()
+//     destination: function(req, file, cb) { //multer esegue queste funzioni ogni volta che un file viene ricevuto
+//         cb(null, './uploads'); // errore e cartella dove mettere il file
+//         cb(null, '');
+//     },
+//     filename: function(req, file, cb) {
+//         let name = file.originalname;
+//         name = name.replace(/ /g, '_');
+//         name = name.replace(/%/g, '_')
+//         cb(null, new Date().toISOString() + name);
+//     }
+// });
+
+const storage = multer.memoryStorage({
+    destination: function(req, res, cb) {
         cb(null, '');
     }
-    // filename: function(req, file, cb) {
-    //     let name = file.originalname;
-    //     name = name.replace(/ /g, '_');
-    //     name = name.replace(/%/g, '_')
-    //     cb(null, new Date().toISOString() + name);
-    // }
 });
 
-let extensionFlag;
-let existFlag;
-
 const fileFilter = (req, file, cb) => {
-    extensionFlag = true;
-    existFlag = true;
-    const { error } = trackValidation(req.body);
-
-    if(error) {
-        cb(null, false);
-        return;
-    }
-
-    if(file.mimetype === undefined) {
-        existFlag = false;
-        return;
-    }
-
     if (file.mimetype === 'application/pdf') {
         cb(null, true); // salva il file
-        return;
-    } 
-    else {
-        cb(extensionFlag = false, false);
-        return;
+    } else {
+        cb(null,false);
+        return cb(new Error('only pdf'));
     }
 }
 
-const upload = multer({ // inizializza multer e dice dove salvare i file
+const upload = multer({
     storage: storage,
     fileFilter: fileFilter
-});
+}).single('music_sheet');
 
-router.post('/', verify, upload.single('music_sheet'), async (req, res) => { // upload.single() solo un file consentito. è un middleware
+router.post('/', verify, async (req, res) => {
 
-    if(!existFlag) {
-        return res.status(400).send({error: 'a file is required'});    
-    }
-
-    if(!extensionFlag) {
-        return res.status(400).send({error: 'only pdf'});
-    }
-
-    const { error } = trackValidation(req.body);
-    if(error) {
-        return res.status(400).send({error: error.details[0].message});
-    }
-
-    const params = {
-        Bucket: process.env.AWS_BUCKET_NAME,
-        Key: `${crypto.randomBytes(20).toString('hex')}.pdf`,
-        Body: req.file.buffer,
-        //ContentType: 'application/pdf',
-        ACL: 'public-read'
-    }
-
-    s3.upload(params, async (err, data) => {
-        if(err) {
-            return res.status(400).send({error: err});
+    upload(req, res, function (err) {
+        if(err instanceof multer.MulterError) {
+            return res.stattus(400).send({error: err.message});
+        } else if (err) {
+            return res.send({error: err.message});
+        }
+        if(!req.file) {
+            return res.status(400).send({error: 'file is required'})
         }
 
-        const link = await data.Location;
-        const user = await User.findById(req.user);
-
-        const track = new Track({
-            title: req.body.title.toLowerCase(),
-            //music_sheet: req.protocol + '://' + req.get('host') + '/' + req.file.path,
-            music_sheet: link,
-            duration: req.body.duration,
-            year_of_composition: req.body.year_of_composition,
-            key: req.body.key.toLowerCase(),
-            video: req.body.video,
-            composer: req.body.composer.toLowerCase(),
-            posted_by: user.name + ' ' + user.surname
-        });
-        try {
-            const savedTrack = await track.save();
-            res.send(savedTrack);
-        } catch(err) {
-            res.status(400).send({error: err});
+        const { error } = trackValidation(req.body);
+        if(error) {
+            return res.status(400).send({error: error.details[0].message});
         }
-    });   
+
+        const params = {
+            Bucket: process.env.AWS_BUCKET_NAME,
+            Key: `${crypto.randomBytes(20).toString('hex')}.pdf`,
+            Body: req.file.buffer,
+            //ContentType: 'application/pdf',
+            ACL: 'public-read'
+        }
+
+        s3.upload(params, async (err, data) => {
+            if(err) {
+                return res.status(400).send({error: err});
+            }
+
+            const link = await data.Location;
+            const user = await User.findById(req.user);
+
+            const track = new Track({
+                title: req.body.title.toLowerCase(),
+                //music_sheet: req.protocol + '://' + req.get('host') + '/' + req.file.path,
+                music_sheet: link,
+                duration: req.body.duration,
+                year_of_composition: req.body.year_of_composition,
+                key: req.body.key.toLowerCase(),
+                video: req.body.video,
+                composer: req.body.composer.toLowerCase(),
+                posted_by: user.name + ' ' + user.surname
+            });
+            try {
+                const savedTrack = await track.save();
+                res.send(savedTrack);
+            } catch(err) {
+                res.status(400).send({error: err});
+            }
+        });   
+    })
 });
 
 module.exports = router;
